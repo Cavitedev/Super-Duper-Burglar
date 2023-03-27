6f48566dfadfa1013533947f24ef5c24f81e3314
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-03-27 19:00:42 +0200

    Crouch and stand up

--- Assets/Project/Scripts/MovePlayer.cs
+++ Assets/Project/Scripts/MovePlayer.cs
@@ -13,6 +13,13 @@ namespace Project.Scripts
 
         private float speed = 0.0f;
 
+        public float crouchDistance = 0.4f;
+        
+        public SteamVR_Action_Boolean crouch = SteamVR_Input.GetBooleanAction("Crouch");
+        public SteamVR_Action_Boolean standUp = SteamVR_Input.GetBooleanAction("StandUp");
+
+        private bool isCrouched = false;
+        
         void Update()
         {
          
@@ -26,6 +33,30 @@ namespace Project.Scripts
 
                 transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
             }
+            
+            bool crouchAction = crouch.GetStateDown(SteamVR_Input_Sources.RightHand);
+            if (crouchAction && !isCrouched)
+            {
+                Crouch();
+            }
+            
+            bool standUp = this.standUp.GetStateDown(SteamVR_Input_Sources.RightHand);
+            if (standUp && isCrouched)
+            {
+                StandUp();
+            }
+        }
+
+        private void Crouch()
+        {
+            transform.position += crouchDistance * Vector3.down;
+            isCrouched = true;
+        }
+        
+        private void StandUp()
+        {
+            transform.position += crouchDistance * Vector3.up;
+            isCrouched = false;
         }
     }
 }
