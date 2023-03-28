f41e01eb02701489f645a3524d848117193f79df
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-03-28 16:50:59 +0200

    Fix Colliding with walls

--- Assets/Project/Scripts/PlayerMovement.cs
+++ Assets/Project/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
@@ -11,30 +12,34 @@ namespace Project.Scripts
         public SteamVR_Action_Vector2 moveValue;
         public float maxSpeed;
         public float sensitivity;
-        public float sphereRadious = 1f;
+        [FormerlySerializedAs("sphereRadious")] public float radious = 1f;
         public float distance;
         public Rigidbody head;
-
-        private float speed = 0.0f;
-
+        public float height = 2f;
         public float crouchDistance = 0.4f;
 
         public SteamVR_Action_Boolean crouch = SteamVR_Input.GetBooleanAction("Crouch");
         public SteamVR_Action_Boolean standUp = SteamVR_Input.GetBooleanAction("StandUp");
 
+        [Header("Debug")]
+        public bool debugMode = false;
+        public Vector2 debugDirection = new Vector3(0, 1);
+
         private bool isCrouched = false;
+
+        private float speed = 0.0f;
 
         void Update()
         {
             MovePlayer();
 
-            bool crouchAction = crouch.GetStateDown(SteamVR_Input_Sources.RightHand);
+            bool crouchAction = crouch.GetStateDown(SteamVR_Input_Sources.RightHand) || Input.GetKeyDown(KeyCode.L);
             if (crouchAction && !isCrouched)
             {
                 Crouch();
             }
 
-            bool standUp = this.standUp.GetStateDown(SteamVR_Input_Sources.RightHand);
+            bool standUp = this.standUp.GetStateDown(SteamVR_Input_Sources.RightHand) || Input.GetKeyDown(KeyCode.O);
             if (standUp && isCrouched)
             {
                 StandUp();
@@ -43,32 +48,51 @@ namespace Project.Scripts
 
         private void MovePlayer()
         {
-            if (moveValue.axis != Vector2.zero)
+            Vector2 moveValueInput;
+            if (debugMode)
+            {
+                moveValueInput = debugDirection;
+            }
+            else
+            {
+                moveValueInput = moveValue.axis;
+            }
+            
+            if (moveValueInput != Vector2.zero)
             {
                 Vector3 direction =
-                    Player.instance.hmdTransform.TransformDirection(new Vector3(moveValue.axis.x, 0,
-                        moveValue.axis.y));
+                    Player.instance.hmdTransform.TransformDirection(new Vector3(moveValueInput.x, 0,
+                        moveValueInput.y)).normalized;
 
-                speed = moveValue.axis.magnitude * sensitivity;
+                speed = moveValueInput.magnitude * sensitivity;
                 speed = Mathf.Clamp(speed, 0, maxSpeed);
 
-                RaycastHit hit;
-
-                Vector3 point1 = transform.position;
-                Vector3 point2 = transform.position;
-                point1.y = 0.2f;
-                point2.y = 1.8f;
-                
-                int mask = LayerMask.NameToLayer("Obstacles");
-                
-                if (Physics.CapsuleCast(point1, point2, 0.5f, direction, out hit, distance, 4))
+                if (!AnyObstacleInDirection(direction))
                 {
-                    Debug.Log("Pared " + hit.transform.gameObject.name);
-                    return;
+                    transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
                 }
-                Debug.Log("Mover");
-                transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
             }
+        }
+
+        private bool AnyObstacleInDirection(Vector3 direction)
+        {
+            RaycastHit hit;
+
+            Vector3 point1 = PositionWihoutCrouch();
+            Vector3 point2 = PositionWihoutCrouch();
+            point1.y -= height/2;
+            point2.y += height/2;
+
+            int maskNumber = LayerMask.NameToLayer("Obstacles");
+            int maskFilter = 1 << maskNumber;
+            if (Physics.CapsuleCast(point1, point2, radious, direction, out hit, distance, maskFilter))
+            {
+                // Debug.Log("obstable " + hit.transform.gameObject.name + " layer: " + hit.transform.gameObject.layer);
+                return true;
+            }
+
+            // Debug.Log("Mover");
+            return false;
         }
 
         private void Crouch()
@@ -83,6 +107,15 @@ namespace Project.Scripts
             isCrouched = false;
         }
 
+        private Vector3 PositionWihoutCrouch()
+        {
+            if (isCrouched)
+            {
+                return transform.position;
+            }
 
+            return transform.position + crouchDistance * Vector3.up;
+            
+        }
     }
 }
