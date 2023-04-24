d8821f6bc7ec49ca1dc210de934e7af6960b32c2
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-04-24 19:28:28 +0200

    UI Start Menu

---
+++ Assets/Project/Scripts/UI/Shared/Laser/LaserPointer.cs
@@ -0,0 +1,38 @@
+//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+using UnityEngine;
+using System.Collections;
+using Valve.VR.Extras;
+
+
+public class LaserPointer : SteamVR_LaserPointer
+{
+    private bool _ignoreInmediateInteraction = false;
+    
+    public void Activate()
+    {
+        pointer.SetActive(true);
+
+    }
+    
+    public void Deactivate()
+    {
+        pointer.SetActive(false);
+        _ignoreInmediateInteraction = true;
+    }
+
+    public override void OnPointerClick(PointerEventArgs e)
+    {
+        if (_ignoreInmediateInteraction)
+        {
+            _ignoreInmediateInteraction = false;
+            return; 
+        }
+        base.OnPointerClick(e);
+    }
+
+    public override void OnPointerOut(PointerEventArgs e)
+    {
+
+        base.OnPointerOut(e);
+    }
+}
---
+++ Assets/Project/Scripts/UI/Shared/Laser/LaserUIHandler.cs
@@ -0,0 +1,86 @@
+﻿
+    using System;
+    using UnityEngine;
+    using Valve.VR;
+    using Valve.VR.Extras;
+
+    public class LaserUIHandler : MonoBehaviour
+    {
+        public LaserPointer rightLaserPointer;
+        public LaserPointer leftLaserPointer;
+
+
+        private SteamVR_LaserPointer[] _laserPointers;
+        
+        void Awake()
+        {
+            _laserPointers = new SteamVR_LaserPointer[] {leftLaserPointer, rightLaserPointer};
+            
+
+            foreach (SteamVR_LaserPointer laserPointer in _laserPointers)
+            {
+                laserPointer.PointerClick += PointerClick;
+                laserPointer.PointerIn += PointerEnter;
+                laserPointer.PointerOut += PointerExit;
+            }
+            
+        }
+
+        private void Update()
+        {
+            UpdateActiveController();
+        }
+
+        private void UpdateActiveController()
+        {
+            if (leftLaserPointer.interactWithUI.GetStateDown(SteamVR_Input_Sources.LeftHand))
+            {
+                leftLaserPointer.enabled = true;
+                leftLaserPointer.Activate();
+                rightLaserPointer.Deactivate();
+                rightLaserPointer.enabled = false;
+
+                // leftLaserPointer.active = true;
+                // rightLaserPointer.active = false;
+            }else if (leftLaserPointer.interactWithUI.GetStateDown(SteamVR_Input_Sources.RightHand))
+            {
+                rightLaserPointer.enabled = true;
+                rightLaserPointer.Activate();
+                leftLaserPointer.Deactivate();
+                leftLaserPointer.enabled = false;
+            }
+        }
+
+        public void PointerClick(object sender, PointerEventArgs e)
+        {
+            IVRButton vrButton = e.target.GetComponent<IVRButton>();
+            
+
+            if (vrButton != null)
+            {
+                vrButton.OnPointerClick(e);
+            }
+        }
+        
+        public void PointerEnter(object sender, PointerEventArgs e)
+        {
+            IVRButton vrButton = e.target.GetComponent<IVRButton>();
+            
+
+            if (vrButton != null)
+            {
+                vrButton.OnPointerEnter(e);
+            }
+        }
+        
+        public void PointerExit(object sender, PointerEventArgs e)
+        {
+            IVRButton vrButton = e.target.GetComponent<IVRButton>();
+            
+
+            if (vrButton != null)
+            {
+                vrButton.OnPointerExit(e);
+            }
+        }
+    }
