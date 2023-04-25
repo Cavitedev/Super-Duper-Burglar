4ab0ccfb81b2babf951e8c697e70a7586b37c2a9
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-04-25 16:41:35 +0200

    Fix Wall and pickable moves towards player

--- Assets/Project/Scripts/Pickable.cs
+++ Assets/Project/Scripts/Pickable.cs
@@ -7,11 +7,6 @@ using Valve.VR.InteractionSystem;
 
 public class Pickable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Called every Update() while a Hand is hovering over this object
     private void HandHoverUpdate(Hand hand)
@@ -19,10 +14,41 @@ public class Pickable : MonoBehaviour
         SteamVR_Input_Sources handType = SteamVR_Input_Sources.Any;
         if (SteamVR_Input.GetStateDown("GrabPinch", handType))
         {
-            
+            Pick();
             
             Debug.Log("Pick");
         }
     }
+
+    private void Pick()
+    {
+        
+        Vector3 playerPos = Player.instance.hmdTransform.transform.position;
+
+        StartCoroutine(movePick(playerPos));
+
+    }
+
+    private IEnumerator movePick(Vector3 goal)
+    {
+        
+
+
+        float goalThreshold = 0.1f;
+        float speed = 0.3f;
+        
+        while (Vector3.SqrMagnitude(goal - transform.position) > goalThreshold)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, goal, speed * Time.deltaTime);
+            yield return null;
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
 
 }
