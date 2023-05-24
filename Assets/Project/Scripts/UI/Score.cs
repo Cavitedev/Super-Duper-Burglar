a15b5d37f13b42f157132295c9c26f98b3319b49
Pablo Domingo Fernández <61729831+Crymxon@users.noreply.github.com>
2023-05-24 18:25:57 +0200

    Game Over fix
    Fixing menu game over

--- Assets/Project/Scripts/UI/Score.cs
+++ Assets/Project/Scripts/UI/Score.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +9,12 @@ public class Score : MonoBehaviour
 
 
     [SerializeField] public PlayerStats amount;
-    public Text score;
+    public TextMeshPro score;
 
     // Start is called before the first frame update
     private void Update()
     {
-        score.text = PlayerStats._bountyAmount.ToString("f0");
+        score.text = PlayerStats.Instance.BountyAmount.ToString("f0");
     }
 
 }
